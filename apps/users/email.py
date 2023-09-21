5858fbb2a6efe34acfcbc2dcbc440588cf6e2faa
Eldiyar1 <mc.oks1ne@gmail.com>
2023-09-22 05:45:06 +0600

    .

--- apps/users/email.py
+++ apps/users/email.py
@@ -10,7 +10,7 @@ def send_email_confirm(email):
     message = f"""Здравствуйте! Ваш адрес электронной почты был указан для входа на приложение Yummy. 
     Пожалуйста, введите этот код на странице авторизации:<<< {random.randint(100000, 999999)} >>> 
     Если это не вы или вы не регистрировались на сайте, то просто проигнорируйте это письмо"""
-    email_from = 'mc.oks1ne@gmail.com'
+    email_from = 'admin@gmail.com'
     send_mail(subject, message, email_from, [email])
     user_obj = CustomUser.objects.get(email=email)
     user_obj.code = random.randint(100000, 999999)
@@ -20,5 +20,5 @@ def send_email_confirm(email):
 def send_email_reset_password(email, recovery_code):
     subject = "Восстановление пароля"
     message = f"Код для восстановления пароля: <<< {recovery_code} >>> Код действителен в течении 5 минут"
-    email_from = 'mc.oks1ne@gmail.com'
+    email_from = 'admin@gmail.com'
     send_mail(subject, message, email_from, [email])
--- core/settings/base.py
+++ core/settings/base.py
@@ -161,5 +161,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "mc.oks1ne@gmail.com"
-EMAIL_HOST_PASSWORD = "pahopmopjsadyxsi"
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
